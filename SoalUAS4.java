b9a3f3d4f161fc538ba99f6994341ebcc124dcec
julia <115854674+julia-kheil@users.noreply.github.com>
2023-01-21 10:42:34 +0700

    Add files via upload

---
+++ SoalUAS4.java
@@ -0,0 +1,20 @@
+package alpro1;
+
+public class SoalUAS4 {
+	public static void main(String[] args) {
+		  int i = 0;
+	       for (int n = 5; n >= 1; n--){
+	           for (int j = n - 0; j >= 0; j--){
+	               System.out.print(" ");
+	           }
+	           for (int d = 0; d <= i; d++){
+	               System.out.print(d);
+	           }
+	           System.out.println();
+	           i += 1;
+	       }
+	   
+	       }
+	   
+	
+}
